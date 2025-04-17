9be1eec546499e026f9be29d81d98eac65603dcf
jvmancilla7 <jmancilla2@unl.edu>
2025-04-17 21:09:06 +0000

    Done with lab 10

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
 #MapPlot.py
-#Name:
-#Date:
-#Assignment:
+#Name:Juan V Mancilla Vargas
+#Date:04/17/2025
+#Assignment: Lab 10- Visualizations with Mathplotlib
+
+
+
+# MapPlot.py
+# Name: Juan V Mancilla Vargas
+# Date: 04/17/2025
+# Assignment: Lab 10 - Visualizations with Matplotlib
+
+
+import pandas
+import matplotlib.pyplot as plt
+
+import construction_spending
+spending = construction_spending.get_spending()
+
+
+years = []
+values = []
+
+
+for item in spending:
+    year = item["time"]["year"]
+    value = item["annual"]["combined"]["commercial"]
+   
+    if value != 0:
+        years.append(year)
+        values.append(value)
+
+
+df = pandas.DataFrame({
+    "Year": years,
+    "Commercial Spending": values
+})
+
+
+print(df)
+
+
+df.plot(kind="line", x="Year", y="Commercial Spending")
+plt.savefig("output")
+
+
